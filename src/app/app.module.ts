ccdc0cd2ae9a362b56b04d6392d65ea77a34d1bf
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-02-02 22:25:15 +0200

    adding component for post

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { NavigationModule } from '@modules/navigation/navigation.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SinglepostComponent } from '../modules/blog/containers/eachpost/singlepost/singlepost.component';
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, SinglepostComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
