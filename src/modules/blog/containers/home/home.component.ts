b61c56d7e8c31601cdb217cba5a283b32c081e42
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-02-04 22:22:49 +0200

    additional error log

--- src/modules/blog/containers/home/home.component.ts
+++ src/modules/blog/containers/home/home.component.ts
@@ -5,6 +5,7 @@ import {
     OnDestroy,
     OnInit,
 } from '@angular/core';
+import {BloggingService} from "@modules/blog/services/blogging.service";
 import { AuthUtilsService } from '@modules/auth/services';
 import { Post } from '@modules/blog/models';
 import { BlogService } from '@modules/blog/services';
@@ -12,21 +13,29 @@ import { Observable, Subscription } from 'rxjs';
 
 @Component({
     selector: 'sb-home',
-    changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: './home.component.html',
     styleUrls: ['home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
-    ngOnInit() {
-        // this.posts$ = this.blogService.getPosts$();
-        //
-        // this.subscription.add(
-        //     this.authUtilsService.isLoggedIn$().subscribe(isLoggedIn => {
-        //         this.isLoggedIn = isLoggedIn;
-        //         this.changeDetectorRef.detectChanges();
-        //     })
-        // );
+    posts: any;
+
+    constructor(private blogService: BloggingService) {
+    }
+    ngOnInit() : void {
+        this.readAll();
     }
 
+    readAll(): void {
+        this.blogService.readAll()
+            .subscribe(
+                posts => {
+                    this.posts = posts;
+                    console.log(posts);
+                },
+                myerrors => {
+                    console.log(myerrors);
+                }
+            )
+    }
 }
--- src/modules/blog/services/blogging.service.ts
+++ src/modules/blog/services/blogging.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+const baseURL = 'http://localhost:8080/getPosts?numberOfPosts=5'
 
 @Injectable({
   providedIn: 'root'
 })
 export class BloggingService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  readAll(): Observable<any>{
+      return this.httpClient.get(baseURL);
+  }
 }
