db2c41157465e36594a98d7c2b507e6d5236e168
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-02-10 22:25:50 +0200

    change baseUrl in service and make it generic

--- src/modules/blog/services/blogging.service.ts
+++ src/modules/blog/services/blogging.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
-const baseURL = 'http://localhost:8080/getPosts?numberOfPosts=5'
+const baseURL = 'http://localhost:8080/'
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +12,9 @@ export class BloggingService {
   constructor(private httpClient: HttpClient) { }
 
   readAll(): Observable<any>{
-      return this.httpClient.get(baseURL);
+      return this.httpClient.get(baseURL+'getPosts?numberOfPosts=5');
   }
+
+
+
 }
