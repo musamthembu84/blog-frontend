eee2ab5d1012d5ce99eab8474ca026f46a30e0ac
musamthembu84@gmail.com <musamthembu84@gmail.com>
2021-04-12 23:46:01 +0200

    enable display of singlepost coming from main component

--- src/modules/blog/containers/eachpost/singlepost.component.ts
+++ src/modules/blog/containers/eachpost/singlepost.component.ts
@@ -8,24 +8,26 @@ import {BloggingService} from "@modules/blog/services/blogging.service";
 })
 export class SinglepostComponent implements OnInit {
 
-    currentPost = null;
+  data = ''
   constructor(private bloggingService:BloggingService) { }
 
   ngOnInit(): void {
+   this.readPost();
   }
 
-  readPost(id : BigInteger): void {
+  readPost(){
+  this.bloggingService.readEachPost(9)
+             .subscribe(
+                 post => {
+                     this.data = post;
 
-      this.bloggingService.readEachPost(id)
-          .subscribe(
-              post => {
-                  this.currentPost = post;
-                  console.log(post);
-              },
-              error => {
-                  console.log(error);
-              }
-          )
-
+                     console.log(JSON.stringify(this.data.title));
+                 },
+                 error => {
+                     console.log(error);
+                 }
+             )
   }
+
+
 }
